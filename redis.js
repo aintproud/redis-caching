2250dea6c9fa1c556843890f8a624bc6901c9412
aintproud <iks.mr2016@yandex.ru>
2022-02-10 21:24:58 +1000

    s

--- redis.js
+++ redis.js
@@ -1,21 +1,10 @@
-const redis = require('redis'),
-client = redis.createClient()
+const redis = require("redis");
+const client = redis.createClient({
+  url: "redis://localhost:1111",
+});
 
-client.on('error', (error)=>console.log(error))
-client.on('connect', ()=>{
-    client.set('random key', 'random value',(error, reply)=>{
-        error? console.log(error):console.log(reply)
-    })
-    client.get('random key', (error, reply)=>{
-        error? console.log(error):console.log(reply)
-    })
-})
-
-
-
-
-
-
-
-
-// console.log(redis, '\n\n\n', client)
+(async () => {
+  await client.connect();
+  await client.set("key", "value");
+  console.log(await client.get("key"));
+})();
