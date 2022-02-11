133f6250a9fd5a2f1f8fee04e82c11677e657f83
aintproud <iks.mr2016@yandex.ru>
2022-02-11 12:35:56 +1000

    final

--- redis.js
+++ redis.js
@@ -6,27 +6,27 @@ const client = redis.createClient({
 (async () => {
   await client.connect()
 
-  .then(console.log(await client.ping()))
+  .then(console.log(await client.ping()))//pong
 
-  .then(console.log(await client.set("key", "value")))
+  .then(console.log(await client.set("key", "value")))//ok
 
-  .then(console.log(await client.get("key")))
+  .then(console.log(await client.get("key")))//value
   
-  .then(console.log(await client.expire("key", 10)))
+  .then(console.log(await client.expire("key", 10)))//true
 
   .then(console.log(await client.hSet("keyOfHash", {'firstKey': 'firstValue', 'secondKey': 'secondValue'}))) //0 if its already created
 
-  .then(console.log(await client.hGet("keyOfHash", 'firstKey')))
+  .then(console.log(await client.hGet("keyOfHash", 'firstKey')))//firstValue
 
-  .then(console.log(await client.hKeys("keyOfHash")))
+  .then(console.log(await client.hKeys("keyOfHash")))//some keys
 
-  .then(console.log(await client.hLen("keyOfHash")))
+  .then(console.log(await client.hLen("keyOfHash")))//length
 
-  .then(console.log(await client.HVALS("keyOfHash")))
+  .then(console.log(await client.HVALS("keyOfHash")))//vals
 
-  .then(console.log(await client.hKeys("keyOfHash")))
+  .then(console.log(await client.hKeys("keyOfHash")))//keys
 
-  .then(console.log(await client.hKeys("keyOfHash")))
+  .then(console.log(await client.hDel("keyOfHash", 'firstKey')))//1
 
   .then(setTimeout(async()=>console.log(await client.keys("key")), 20000))//sync
 
